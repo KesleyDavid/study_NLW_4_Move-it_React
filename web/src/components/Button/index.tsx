3f013a6335826d96aaa915dec8322645453f02ad
Kesley DEV <kesleygti@gmail.com>
2021-02-26 09:53:34 -0300

    [web] [start] Create App Typescript

---
+++ web/src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Button: React.FC = () => {
+  return <S.Container />;
+}
+
+export default Button;
---
+++ web/src/components/Button/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: red;
+  height: 100px;
+  width: 100px;
+`; 
